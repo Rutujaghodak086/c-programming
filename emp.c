415898524180bfd35729477337c079cb1a16a279
Rutuja ghodake <ghodakerutuja086@gmail.com>
2025-06-10 21:00:52 +0530

    emp commit

--- emp.c
+++ emp.c
@@ -2,15 +2,31 @@
 int main()
 {
     float  bs,da,hra,tax,totalsalary;
-    printf("Enter Basic salary");
+    int eid;
+    char ename[100];
+    FILE *fp;
+    fp = fopen("salarySlip.tex","w");
+    printf("Enter Employee Details :");
+    printf("\nID");
+    scanf("%d",&eid);
+    printf("\nName");
+    scanf("%s",&ename);
+    printf("Enter Basic salary:");
     scanf("%f",&bs);
-    printf("Enter daliy alouns");
+    printf("Enter daliy alouns(%%):");
     scanf("%f",&da);
-    printf(" enter house rent");
+    printf(" enter house rent(%%):");
     scanf("%f",&hra);
-    printf("Enter Tax");
+    printf("Enter Tax(%%):");
     scanf("%f",&tax);
     totalsalary=bs+((bs*(da+hra-tax))/100);
-    printf("total salary %.2f",totalsalary);
+    fprintf(fp,"\n-----------Employee salary Slip----------");
+    fprintf(fp,"\n\t\tID          :%d",eid);
+    fprintf(fp,"\n\t\tName        :%s",ename);
+    fprintf(fp,"\n\t\tBasic salary:%.2f",bs);
+    fprintf(fp,"\n\t\tDA (%%)     :%.2f ",da);
+    fprintf(fp,"\n\t\tHRA(%%)     :%.2f",hra);
+    fprintf(fp,"\n\t\tTax(%%)     :%.2f",tax);
+    fprintf(fp,"\n\t\ttotal salary         :%.2f",totalsalary);
 return 0;
 }
